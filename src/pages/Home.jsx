dd7412a77cb370cb123b8931e86efb738b9ffee4
Lingrode <grimaloilya@gmail.com>
2024-06-28 22:36:51 -0600

    async action in Redux Toolkit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
 
-import axios from "../axios";
-
 import { Post } from "../components/Post";
 import { TagsBlock } from "../components/TagsBlock";
 import { CommentsBlock } from "../components/CommentsBlock";
+import { fetchPosts } from "../redux/slices/posts";
 
 export const Home = () => {
+  const dispatch = useDispatch();
+
   React.useEffect(() => {
-    axios.get("/posts");
-  }, []);
+    dispatch(fetchPosts());
+  }, [dispatch]);
 
   return (
     <>
