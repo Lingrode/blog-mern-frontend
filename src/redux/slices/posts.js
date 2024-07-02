af6c2ccb11207c6159de45fe75c43aad9173ffe9
Lingrode <grimaloilya@gmail.com>
2024-07-01 21:21:34 -0600

    saving the status of the request and article to the state

--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -20,7 +20,21 @@ const initialState = {
 const postSlice = createSlice({
   name: "posts",
   initialState,
-  reducer: {},
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.posts.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.posts.items = action.payload;
+        state.posts.status = "loaded";
+      })
+      .addCase(fetchPosts.rejected, (state) => {
+        state.posts.items = [];
+        state.posts.status = "error";
+      });
+  },
 });
 
 export const postsReducer = postSlice.reducer;
