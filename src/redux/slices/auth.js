aa366ad01e38f25f9409947fec42acd556713e74
Lingrode <grimaloilya@gmail.com>
2024-07-03 18:33:22 -0600

    checking whether the user is authorized or not and added logout

--- src/redux/slices/auth.js
+++ src/redux/slices/auth.js
@@ -14,6 +14,11 @@ const initialState = {
 const authSlice = createSlice({
   name: "auth",
   initialState,
+  reducers: {
+    logout: (state) => {
+      state.data = null;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchAuth.pending, (state) => {
@@ -31,4 +36,8 @@ const authSlice = createSlice({
   },
 });
 
+export const selectIsAuth = (state) => Boolean(state.auth.data);
+
 export const authReducer = authSlice.reducer;
+
+export const { logout } = authSlice.actions;
