e1533b5c4ec77a13fc96890dab6f874dc1ee500a
Lingrode <grimaloilya@gmail.com>
2024-07-09 20:10:03 -0600

    render artciles with React Markdown

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "../axios";
 import { useParams } from "react-router-dom";
+import Markdown from "react-markdown";
 
 import { Post } from "../components/Post";
 import { Index } from "../components/AddComment";
@@ -34,7 +35,7 @@ export const FullPost = () => {
         id={data._id}
         title={data.title}
         // imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        imageUrl={data.imageUrl}
+        imageUrl={`http://localhost:4444${data.imageUrl}`}
         user={data.user}
         createdAt={data.createdAt}
         viewsCount={data.viewsCount}
@@ -42,7 +43,7 @@ export const FullPost = () => {
         tags={data.tags}
         isFullPost
       >
-        <p>{data.text}</p>
+        <Markdown>{data.text}</Markdown>
       </Post>
       <CommentsBlock
         items={[
